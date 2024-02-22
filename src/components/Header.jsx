60dca76a6e67c0ccae855d7c4303e1b5c04edd8d
Thiha Han <thih41232@gmail.com>
2024-02-22 19:53:13 +0630

    added navbar

---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+export default function Header(){
+    return (
+        <div className="py-4 px-3 border border-b-1">
+            <ul className="list flex gap-3">
+                <li className="list-item nav-item">Projects</li>
+                <li className="list-item nav-item">About</li>
+                <li className="list-item nav-item">Contact Me</li>
+            </ul>
+        </div>
+    )
+}
